ad92cf6a9a72d7edac575b7acc0f75aaf2ca4229
Gabriela Barreto <gabriela@MacBook-Pro-de-Rodrigo.local>
2026-09-19 19:08:23 -0300

    Atualiza a foto da Dora Feijóo.

--- script.js
+++ script.js
@@ -130,7 +130,7 @@ function saveAll() {
   localStorage.setItem(STORAGE_PHOTOS, JSON.stringify(customPhotos));
 }
 
-const PHOTO_VERSION = "3";
+const PHOTO_VERSION = "4";
 
 function photoSrc(photo) {
   if (customPhotos[photo]) return customPhotos[photo];
